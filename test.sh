f0cd038917482557fb5696ff2c90828bcc74e2d5
Shoichiro Kawauchi <lacrosse91.ut@gmail.com>
2020-07-06 09:09:39 +0000

    セミコロンの導入

--- test.sh
+++ test.sh
@@ -16,35 +16,36 @@ assert() {
   fi
 }
 
-assert 0 0
-assert 42 42
-assert 21 '5+20-4'
-assert 15 '10 + 8 - 3'
-assert 6 '2 * 3'
-assert 18 '2 + 4 * 4'
-assert 13 '12 / 4 + 5 * 2'
-assert 14 '2 * (3 + 4)'
-assert 15 '1 + 2 + 3 + 4 + 5'
-assert 6 '+6'
-assert 4 '-8 + 12'
+assert 0 '0;'
+assert 42 '42;'
+assert 21 '5+20-4;'
+assert 41 ' 12 + 34 - 5 ;'
+assert 47 '5+6*7;'
+assert 15 '5*(9-6);'
+assert 4 '(3+5)/2;'
+assert 10 '-10+20;'
+assert 10 '- -10;'
+assert 10 '- - +10;'
 
-assert 0 '0==1'
-assert 1 '42==42'
-assert 1 '0!=1'
-assert 0 '42!=42'
+assert 0 '0==1;'
+assert 1 '42==42;'
+assert 1 '0!=1;'
+assert 0 '42!=42;'
 
-assert 1 '0<1'
-assert 0 '1<1'
-assert 0 '2<1'
-assert 1 '0<=1'
-assert 1 '1<=1'
-assert 0 '2<=1'
+assert 1 '0<1;'
+assert 0 '1<1;'
+assert 0 '2<1;'
+assert 1 '0<=1;'
+assert 1 '1<=1;'
+assert 0 '2<=1;'
 
-assert 1 '1>0'
-assert 0 '1>1'
-assert 0 '1>2'
-assert 1 '1>=0'
-assert 1 '1>=1'
-assert 0 '1>=2'
+assert 1 '1>0;'
+assert 0 '1>1;'
+assert 0 '1>2;'
+assert 1 '1>=0;'
+assert 1 '1>=1;'
+assert 0 '1>=2;'
+
+assert 3 '1; 2; 3;'
 
 echo OK
